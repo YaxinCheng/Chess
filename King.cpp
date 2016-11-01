6f64de9b875163e53ec81950221eb1e3fa721eb5
Yaxin Cheng <yx299612@dal.ca>
2016-11-01 18:00:49 -0300

    Add diagonal move support for Kings

--- King.cpp
+++ King.cpp
@@ -1,15 +1,17 @@
 #include "King.h"
 
-King::King(short x, short y, bool colour): Piece(x, y, colour), StraightMove() {
+King::King(short x, short y, bool colour): Piece(x, y, colour), StraightMove(), DiagonalMove() {
 }
 
 King::~King() {}
 
 const bool King::checkLegal(short x, short y) {
-	if (isStraight(this->x, this->y, x, y) == false ) {
-		return false;
+	if (isStraight(this->x, this->y, x, y)) {
+		return abs(x - this->x) + abs(y - this->y) == 1;
+	} else if (isDiagonal(this->x, this->y, x, y)) {
+		return abs(x - this->x) + abs(y - this->y) == 2;
 	}
-	return abs(x - this->x) + abs(y - this->y) == 1;
+	return false;
 }
 
 const string King::icon() {
--- King.h
+++ King.h
@@ -2,10 +2,11 @@
 #define KING_H
 #include "Piece.h"
 #include "StraightMove.h"
+#include "DiagonalMove.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-struct King: Piece, StraightMove {
+struct King: Piece, StraightMove, DiagonalMove {
 	King(short x, short y, bool colour);
 	~King();
 	const bool checkLegal(short x, short y);
